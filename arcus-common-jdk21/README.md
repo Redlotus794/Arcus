a213c3b919d13b35e3fb8904f4a9ae80c5bdabae
wangjialong <jialong.wang@smil.com>
2025-06-20 17:35:24 +0800

    新增 Arcus Common 模块，包含自定义运行时异常和响应体类；更新 POM 文件以管理依赖

---
+++ arcus-common-jdk21/README.md
@@ -0,0 +1,4 @@
+# Arcus Common
+
+旨在基于DDD（领域驱动设计）思想下，提供跨Bounded Context的共享内核（Shared Kernel）。只包含真正在多个子域之间都依赖的稳定的，无业务耦合的代码。
+
---
+++ arcus-sso/src/main/java/com/rdlts/arcus/sso/ArcusSSOApplication.java
@@ -0,0 +1,12 @@
+package com.rdlts.arcus.sso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArcusSSOApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArcusSSOApplication.class, args);
+    }
+}
