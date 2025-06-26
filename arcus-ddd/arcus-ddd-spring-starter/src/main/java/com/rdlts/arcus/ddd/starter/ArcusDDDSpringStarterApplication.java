69e3ad0b97598143ea4f3e014eb30de001db167b
wangjialong <jialong.wang@smil.com>
2025-06-26 13:10:09 +0800

    新增 Arcus DDD 核心模块，包含领域服务和领域服务注册接口；更新 POM 文件以管理依赖和模块结构

---
+++ arcus-ddd/arcus-ddd-spring-starter/src/main/java/com/rdlts/arcus/ddd/starter/ArcusDDDSpringStarterApplication.java
@@ -0,0 +1,12 @@
+package com.rdlts.arcus.ddd.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArcusDDDSpringStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArcusDDDSpringStarterApplication.class, args);
+    }
+}
---
+++ arcus-ddd/arcus-ddd-test/README.md
@@ -0,0 +1,11 @@
+# Arcus DDD Test
+
+# Quick Start Guide
+基于Junit5 的 java bdd测试框架，提供了领域驱动设计的测试支持。 
+
+
+```shell
+
+```
+
+#
