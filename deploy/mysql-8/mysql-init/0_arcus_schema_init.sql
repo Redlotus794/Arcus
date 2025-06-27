5c3f79d369ca771f29f52eec78aba6a7ee460def
wangjialong <jialong.wang@smil.com>
2025-06-27 13:46:44 +0800

    新增数据库初始化脚本和用户权限授予脚本，支持 arcus-identity 数据库的创建与权限管理

---
+++ deploy/mysql-8/mysql-init/0_arcus_schema_init.sql
@@ -0,0 +1,4 @@
+-- database arcus-identity
+CREATE DATABASE IF NOT EXISTS `arcus-identity`
+  CHARACTER SET utf8mb4
+  COLLATE utf8mb4_bin;
---
+++ deploy/mysql-8/mysql-init/1_grant_user_permission.sql
@@ -0,0 +1,4 @@
+-- grant by root
+-- arcus user permission
+GRANT ALL PRIVILEGES ON `arcus-identity`.* TO 'arcus'@'%';
+FLUSH PRIVILEGES;
