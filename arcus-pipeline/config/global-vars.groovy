064743ddab64af4f94e553b6a4580b560b05de10
wangjialong <jialong.wang@smil.com>
2025-08-01 10:50:48 +0800

    修正 ARCUS_GIT_REPO_URL 的拼写错误

--- arcus-pipeline/config/global-vars.groovy
+++ arcus-pipeline/config/global-vars.groovy
@@ -4,7 +4,7 @@
 // 返回一个包含所有全局变量的 Map
 
 return [
-    ARCUS_GIT_REPO_URL: "https://github.com/Redlotus794/Arucs.git",
+    ARCUS_GIT_REPO_URL: "https://github.com/Redlotus794/Arcus.git",
     // K8s 部署配置
     K8S_DEPLOY_CONFIG: "k8s/deployment.yaml",
 
