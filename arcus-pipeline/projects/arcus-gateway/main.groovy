0cf487ee7408ed82939f41c9cbdf62b4c60c06af
wangjialong <jialong.wang@smil.com>
2025-08-01 09:11:34 +0800

    移除构建和部署模板中的不必要参数

--- arcus-pipeline/projects/arcus-gateway/main.groovy
+++ arcus-pipeline/projects/arcus-gateway/main.groovy
@@ -1,9 +1,6 @@
 @Library('shared-libs') _
 
 buildAndDeployTemplate_20250801([
-        gitBranch: 'main',
         imageName: 'arcus-gateway',
-        imageTag: 'lts',
-        namespace: 'arcus',
         projectDir: 'arcus-gateway',
 ])
